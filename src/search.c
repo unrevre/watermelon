2f369274f7dde9c1ea039d6c108a94cd3dbe3da7
unrevre <unrevre@gmail.com>
2018-12-28 20:52:20 -0500

    search: print fen string before recursive call
    more intuitive debug output for node traversal.

--- src/search.c
+++ src/search.c
@@ -29,12 +29,13 @@ move_t iter_dfs(uint32_t depth, uint32_t side) {
          move(moves.data[i]);
 #ifdef DEBUG
          ++nodes;
+         char* fen_str = info_fen();
+         printf("fen: %s\n", fen_str);
 #endif
 
          int32_t score = -negamax(d - 1, 1, -beta, -alpha, side ^ 0x8);
 
 #ifdef DEBUG
-         char* fen_str = info_fen();
          printf("fen: %s\n", fen_str);
          free(fen_str);
          printf("  score: %i [%i, %i]\n", score, alpha, beta);
@@ -68,14 +69,18 @@ int32_t negamax(uint32_t depth, uint32_t ply, int32_t alpha, int32_t beta,
       move(moves.data[i]);
 #ifdef DEBUG
       ++nodes;
+      for (uint32_t t = 0; t < ply; ++t) { printf(" "); }
+      char* fen_str = info_fen();
+      printf("fen: %s\n", fen_str);
 #endif
 
       int32_t score = -negamax(depth - 1, ply + 1, -beta, -alpha, side ^ 0x8);
 
 #ifdef DEBUG
-      for (uint32_t t = 0; t < ply; ++t) { printf(" "); }
-      char* fen_str = info_fen();
-      printf("fen: %s\n", fen_str);
+      if (depth > 1) {
+         for (uint32_t t = 0; t < ply; ++t) { printf(" "); }
+         printf("fen: %s\n", fen_str);
+      }
       free(fen_str);
       for (uint32_t t = 0; t < ply; ++t) { printf(" "); }
       printf("  score: %i [%i, %i]\n", score, alpha, beta);
