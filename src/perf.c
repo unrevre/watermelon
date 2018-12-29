24fd06f8833b9ca9e46b6e870579b5853e038559
unrevre <unrevre@gmail.com>
2018-12-29 00:15:01 -0500

    perf: add perft for capture move generation

--- src/perf.c
+++ src/perf.c
@@ -17,3 +17,19 @@ uint64_t perft(uint32_t depth, uint32_t side) {
 
    return nmoves;
 }
+
+uint64_t perft_capture(uint32_t depth, uint32_t side) {
+   if (!depth) { return 1; }
+
+   uint64_t nmoves = 0;
+   move_array_t moves = generate_captures(side);
+   for (uint32_t i = 0; i != moves.count; ++i) {
+      move(moves.data[i]);
+      if (!in_check(side))
+         nmoves += perft_capture(depth - 1, side ^ 0x8);
+      retract(moves.data[i]);
+   }
+   free(moves.data);
+
+   return nmoves;
+}
--- src/perf.h
+++ src/perf.h
@@ -11,4 +11,11 @@
 
 uint64_t perft(uint32_t depth, uint32_t side);
 
+/*!
+ * perft_capture
+ * = number of legal capture moves
+ */
+
+uint64_t perft_capture(uint32_t depth, uint32_t side);
+
 #endif /* PERF_H */
