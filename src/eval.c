dd96f694a2299379d047a9d1fd488ac3da0a3b5b
unrevre <unrevre@gmail.com>
2018-12-30 12:00:28 -0500

    eval: add material evaluation
    weighted popcount

--- src/eval.c
+++ src/eval.c
@@ -3,9 +3,13 @@
 #include "inlines.h"
 #include "state.h"
 
+static uint32_t weights[7] = {32, 16, 16, 4, 4, 4, 4096};
+
 int32_t eval(uint32_t side) {
-   int32_t popdiff = popcnt(GAME.occupancy[side >> 0x3])
-      - popcnt(GAME.occupancy[!side]);
+   int32_t popdiff = 0;
+   for (uint32_t i = 0; i < 7; ++i)
+      popdiff += weights[i] * (popcnt(GAME.pieces[side + i])
+         - popcnt(GAME.pieces[(side ^ 0x8) + i]));
 
    return popdiff;
 }
