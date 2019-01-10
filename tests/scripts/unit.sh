6513febbd6621e160155fcca3ce13f4ee2f90074
unrevre <unrevre@gmail.com>
2019-01-10 17:16:24 -0500

    tests: refactor tests structure
    add generic script to execute any unit test requiring depth, fen string
    arguments
    script may be executed from any directory
    
    modify .travis.yml accordingly

---
+++ tests/scripts/unit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+[ $# -ne 3 ] && {
+   echo "  usage: $BASH_SOURCE [unit] [depth] [input]";
+   exit 1;
+}
+
+unit=$1
+depth=$2
+input=$3
+
+while read fen; do
+   [ $fen ] && $unit $depth $fen || echo
+done <<< "$(cat $input)"
