adb61ea937f377616b4b569ece741e51ff918345
unrevre <unrevre@gmail.com>
2019-01-11 22:17:13 -0500

    eval: evaluate from fixed perspective
    and return conditionally based on side
    'cmovel' instruction is emitted with clang-9.0.0

--- src/eval.c
+++ src/eval.c
@@ -8,8 +8,8 @@ static uint32_t weights[7] = {4096, 32, 16, 16, 4, 4, 4};
 int32_t eval(uint32_t side) {
    int32_t popdiff = 0;
    for (uint32_t i = 0; i < 7; ++i)
-      popdiff += weights[i] * (popcnt(game.pieces[ps(side, i)])
-         - popcnt(game.pieces[po(side, i)]));
+      popdiff += weights[i] * (popcnt(game.pieces[ps(0x0, i)])
+         - popcnt(game.pieces[po(0x0, i)]));
 
-   return popdiff;
+   return side ? -popdiff : popdiff;
 }
