c42bbea2e27846261ed3257611deb927bb6724e4
unrevre <unrevre@gmail.com>
2019-01-17 23:56:46 -0500

    fen: minor sanitisation of code
    simplify, compactify fen parsing code
    effectively no change

--- src/fen.c
+++ src/fen.c
@@ -31,60 +31,44 @@ uint32_t init_fen(const char* fen_str) {
    for (uint32_t i = 0; i < 10; ++i)
       strcat(fstr_cat, lines[i]);
 
-   game.occupancy[0] = 0x0;
-   game.occupancy[1] = 0x0;
-
    for (uint32_t i = 0; i < 15; ++i)
       game.pieces[i] = 0x0;
-
    for (uint32_t i = 0; i < 90; ++i)
       board[i] = empty;
 
    char* fstr_p = fstr_cat;
    for (uint32_t i = 0; *fstr_p; ++fstr_p, ++i) {
-      uint32_t side = (*fstr_p > 'Z');
+      int32_t piece = -1;
+
       switch (*fstr_p) {
          case '1': case '2': case '3': case '4': case '5':
          case '6': case '7': case '8': case '9':
-            i += *fstr_p - '1';
-            break;
-         case 'k': case 'K':
-            game.pieces[ps(side, 0x0)] |= PMASK[i];
-            board[i] = ps(side, 0x0);
-            break;
-         case 'r': case 'R':
-            game.pieces[ps(side, 0x1)] |= PMASK[i];
-            board[i] = ps(side, 0x1);
-            break;
-         case 'n': case 'N':
-            game.pieces[ps(side, 0x2)] |= PMASK[i];
-            board[i] = ps(side, 0x2);
-            break;
-         case 'c': case 'C':
-            game.pieces[ps(side, 0x3)] |= PMASK[i];
-            board[i] = ps(side, 0x3);
-            break;
-         case 'a': case 'A':
-            game.pieces[ps(side, 0x4)] |= PMASK[i];
-            board[i] = ps(side, 0x4);
-            break;
-         case 'b': case 'B':
-            game.pieces[ps(side, 0x5)] |= PMASK[i];
-            board[i] = ps(side, 0x5);
-            break;
-         case 'p': case 'P':
-            game.pieces[ps(side, 0x6)] |= PMASK[i];
-            board[i] = ps(side, 0x6);
-            break;
-         default:
-            printf("invalid char in FEN str\n");
+            i += *fstr_p - '1'; break;
+         case 'k': case 'K': piece = 0x0; break;
+         case 'r': case 'R': piece = 0x1; break;
+         case 'n': case 'N': piece = 0x2; break;
+         case 'c': case 'C': piece = 0x3; break;
+         case 'a': case 'A': piece = 0x4; break;
+         case 'b': case 'B': piece = 0x5; break;
+         case 'p': case 'P': piece = 0x6; break;
+         default: printf(" [fen] invalid char!\n");
+      }
+
+      if (piece > -1) {
+         uint32_t side = (*fstr_p > 'Z');
+         game.pieces[ps(side, piece)] |= PMASK[i];
+         board[i] = ps(side, piece);
       }
    }
+
+   game.occupancy[0] = 0x0;
+   game.occupancy[1] = 0x0;
 
    for (uint32_t i = 0; i < 7; ++i) {
       game.occupancy[0] |= game.pieces[ps(0x0, i)];
       game.occupancy[1] |= game.pieces[po(0x0, i)];
    }
+
    game.pieces[empty] = ~(game.occupancy[0] | game.occupancy[1]);
 
    for (uint32_t i = 0; i < 10; ++i)
@@ -92,13 +76,10 @@ uint32_t init_fen(const char* fen_str) {
    free(fstr_cat);
 
    switch (schar) {
-      case 'r':
-         return RSIDE;
-      case 'b':
-         return BSIDE;
+      case 'r': return RSIDE;
+      case 'b': return BSIDE;
+      default: return RSIDE;
    }
-
-   return RSIDE;
 }
 
 char* info_fen(uint32_t side) {
