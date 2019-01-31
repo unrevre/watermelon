f1e3ed76057997072542b4728f742a83665288ef
unrevre <unrevre@gmail.com>
2019-01-31 12:20:58 -0500

    search, state: promote ply to global variable
    with consistent handling of null moves, ply can be tracked by the state
    advance/retract functions

--- src/search.h
+++ src/search.h
@@ -17,22 +17,21 @@ move_t iter_dfs(uint32_t depth, uint32_t side);
  * @ negamax search, with alpha-beta pruning (fail-soft)
  */
 
-int32_t negamax(uint32_t depth, uint32_t ply, int32_t alpha, int32_t beta,
-                uint32_t side);
+int32_t negamax(uint32_t depth, int32_t alpha, int32_t beta, uint32_t side);
 
 /*!
  * quiescence
  * @ quiescence search (fail-soft)
  */
 
-int32_t quiescence(uint32_t ply, int32_t alpha, int32_t beta, uint32_t side);
+int32_t quiescence(int32_t alpha, int32_t beta, uint32_t side);
 
 /*!
  * store_hash
  * @ transposition table replacement scheme
  */
 
-void store_hash(uint32_t depth, uint32_t ply, int32_t alpha, int32_t beta,
-                int32_t score, move_t move_hash);
+void store_hash(uint32_t depth, int32_t alpha, int32_t beta, int32_t score,
+                move_t move_hash);
 
 #endif /* SEARCH_H */
