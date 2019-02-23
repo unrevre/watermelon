97d3038eb25f428272b485661d4f5fc9d6d3dee8
unrevre <unrevre@gmail.com>
2019-02-23 11:33:10 -0500

    memory: refactor hash probe/store functions
    iterate only once over entries in a basket

--- src/memory.c
+++ src/memory.c
@@ -27,36 +27,25 @@ void store_hash(int32_t depth, int32_t alpha, int32_t beta, int32_t score,
    score = (score > WSCORE - PLYLIMIT) ? score + state.ply :
       (score < -LSCORE + PLYLIMIT) ? score - state.ply : score;
 
-   uint32_t replace = HASHSIZE;
-   uint32_t index = state.hash & HASHMASK;
-   for (uint32_t t = 0; t != BASKETS; ++t) {
-      uint32_t entry = index ^ t;
-      if (!ttable[entry].bits) {
-         replace = entry;
-         break;
-      } else if (ttable[entry]._.hash == state.hash >> HASHBITS) {
-         replace = entry;
-         break;
-      } else if (ttable[entry]._.age != (state.step & 0x3)) {
-         replace = entry;
-      }
-   }
-
-   if (replace == HASHSIZE) {
-      uint32_t min_depth = PLYLIMIT;
-
-      replace = index;
-      for (uint32_t t = 0; t != BASKETS; ++t) {
-         uint32_t entry = index ^ t;
-         if (ttable[entry]._.depth < min_depth) {
-            min_depth = ttable[entry]._.depth;
-            replace = entry;
-         }
-      }
-   }
-
-   ttable[replace] = (ttentry_t) { ._ = {
+   ttentry_t new = { ._ = {
       state.hash >> HASHBITS, flags, depth, score, state.step, move } };
+
+   int64_t age_prefer = -1;
+   int64_t depth_prefer = state.hash & HASHMASK;
+   for (uint32_t t = 0; t != BASKETS; ++t) {
+      uint32_t index = (state.hash & HASHMASK) ^ t;
+      if (!ttable[index].bits
+            || ttable[index]._.hash == state.hash >> HASHBITS) {
+         ttable[index] = new; return; }
+
+      if (ttable[index]._.age != (state.step & 0x3)) {
+         age_prefer = index; }
+      if (ttable[index]._.depth < ttable[depth_prefer]._.depth) {
+         depth_prefer = index; }
+   }
+
+   int64_t replace = age_prefer >= 0 ? age_prefer : depth_prefer;
+   ttable[replace] = new;
 }
 
 int32_t probe_hash(int32_t depth, int32_t* alpha, int32_t* beta,
@@ -94,15 +83,12 @@ int32_t probe_hash(int32_t depth, int32_t* alpha, int32_t* beta,
 }
 
 ttentry_t probe_hash_for_entry() {
-   ttentry_t entry = {0};
    for (uint32_t t = 0; t != BASKETS; ++t) {
       uint32_t index = (state.hash & HASHMASK) ^ t;
       if (ttable[index]._.hash == state.hash >> HASHBITS
             && ttable[index]._.flags == FEXACT) {
-         entry = ttable[index];
-         break;
-      }
+         return ttable[index]; }
    }
 
-   return entry;
+   return (ttentry_t){0};
 }
