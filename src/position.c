43bcab9bfeecc1cddb76972e8cc7af74f8a66b9a
unrevre <unrevre@gmail.com>
2019-02-24 21:33:18 -0500

    position: compare consecutive hashes together
    trick compiler by casting to uint64_t

--- src/position.c
+++ src/position.c
@@ -127,8 +127,8 @@ uint32_t is_legal(move_t move) {
 }
 
 uint32_t is_repetition() {
-   return state.step > 4 ? (htable[state.step] == htable[state.step - 4]
-      && htable[state.step - 1] == htable[state.step - 5]) : 0;
+   return state.step > 4 ? *((uint64_t*)(htable + state.step - 1))
+      == *((uint64_t*)(htable + state.step - 5)) : 0;
 }
 
 uint32_t is_index_movable(int32_t index) {
