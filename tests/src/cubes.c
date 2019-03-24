8175fec521c5efc7e340eb50e893cff2c8fd9444
unrevre <unrevre@gmail.com>
2019-03-24 00:03:32 -0400

    tests: add playtesting binary
    takes two full executable commands excluding fen string (a separate
    third argument)

---
+++ tests/src/cubes.c
@@ -0,0 +1,98 @@
+#include "../../src/fen.h"
+#include "../../src/magics.h"
+#include "../../src/utilities.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+pid_t childpid[2] = {0};
+int fd[2][2] = {{0, 1}};
+
+void taste(int64_t side, char* buffer);
+void serve(char* args[], int64_t side);
+
+int main(int argc, char* argv[]) {
+   if (argc != 4) {
+      printf("usage: %s [red] [black] [fen]\n", argv[0]);
+      return 1;
+   }
+
+   serve(append(slice(argv[1]), argv[3]), red);
+   serve(append(slice(argv[2]), argv[3]), black);
+
+   printf("served!\n");
+   printf("  pids: %i, %i\n\n", childpid[0], childpid[1]);
+
+   char buffer[256];
+   sprintf(buffer, "\n");
+
+   for (int64_t side = side_from(argv[3]); ; side = o(side)) {
+      taste(side, buffer);
+      printf("[%c] %s", fen_side[side], buffer);
+   }
+
+   return 0;
+}
+
+void taste(int64_t side, char* buffer) {
+   write(fd[side][1], buffer, strlen(buffer));
+   write(fd[side][1], "next\n", 5);
+
+   fd_set set;
+   FD_ZERO(&set);
+   FD_SET(fd[side][0], &set);
+
+   select(fd[side][0] + 1, &set, NULL, NULL, NULL);
+   int32_t nbytes = read(fd[side][0], buffer, 256);
+   buffer[nbytes] = '\0';
+
+   sleep(1);
+
+   int32_t status;
+   if (waitpid(-1, &status, WNOHANG)) {
+      printf("%c loss\n", fen_side[side]);
+      exit(1);
+   }
+
+   char** tokens = slice(buffer);
+   if (tokens[0] && tokens[0][0] != '\0' && tokens[0][1] == ':') {
+      int32_t from = atoi(tokens[1]);
+      int32_t to = atoi(tokens[3]);
+
+      sprintf(buffer, "move %i %i\n", from , to);
+   } else {
+      printf("fatal: invalid read\n");
+      exit(1);
+   }
+}
+
+void serve(char* args[], int64_t side) {
+   int32_t r_pipe[2];
+   int32_t w_pipe[2];
+
+   pipe(r_pipe);
+   pipe(w_pipe);
+
+   childpid[side] = fork();
+
+   if (!childpid[side]) {
+      dup2(r_pipe[1], 1);
+      dup2(w_pipe[0], 0);
+
+      close(r_pipe[0]);
+      close(w_pipe[1]);
+
+      execvp(args[0], args);
+   } else {
+      close(r_pipe[1]);
+      close(w_pipe[0]);
+
+      fd[side][0] = r_pipe[0];
+      fd[side][1] = w_pipe[1];
+   }
+
+   free(args);
+}
