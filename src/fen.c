5a1405f6eddc95552b26406f332e82160d700fdc
unrevre <unrevre@gmail.com>
2019-03-26 16:40:17 -0400

    fen: fix parsing order
    parsing resulted in mirror image of board position

--- src/fen.c
+++ src/fen.c
@@ -27,13 +27,13 @@ void reset_fen(const char* fen_str) {
       board[i] = empty;
 
    char* fstr_p = fstr;
-   for (int64_t i = 89; *fstr_p; ++fstr_p, --i) {
+   for (int64_t i = 81; *fstr_p; ++fstr_p, ++i) {
       int32_t piece = -1;
 
       switch (*fstr_p) {
          case '1': case '2': case '3': case '4': case '5':
          case '6': case '7': case '8': case '9':
-            i -= *fstr_p - '1'; break;
+            i += *fstr_p - '1'; break;
          case 'k': case 'K': piece = 0x0; break;
          case 'r': case 'R': piece = 0x1; break;
          case 'n': case 'N': piece = 0x2; break;
@@ -41,7 +41,7 @@ void reset_fen(const char* fen_str) {
          case 'a': case 'A': piece = 0x4; break;
          case 'b': case 'B': piece = 0x5; break;
          case 'p': case 'P': piece = 0x6; break;
-         case '/': ++i; break;
+         case '/': i -= 19; break;
       }
 
       if (piece != -1) {
