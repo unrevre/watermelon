26967f88d21282356a7727196c131491cd10bfc5
unrevre <unrevre@gmail.com>
2019-04-17 15:14:35 -0400

    perf: wrap perft implementations
    internally enforce consistency of side-to-move

--- src/perf.c
+++ src/perf.c
@@ -7,7 +7,12 @@
 
 #include <stdlib.h>
 
-int64_t perft(int32_t depth, int64_t side) {
+/*!
+ * impl_perft
+ * @ internal implementation of perft
+ */
+
+int64_t impl_perft(int32_t depth, int64_t side) {
    if (!depth) { return 1; }
 
    int64_t nmoves = 0;
@@ -15,7 +20,7 @@ int64_t perft(int32_t depth, int64_t side) {
    for (int64_t i = 0; i != moves.count; ++i) {
       advance_board(moves.data[i]);
       if (!in_check(side))
-         nmoves += perft(depth - 1, side ^ pass);
+         nmoves += impl_perft(depth - 1, side ^ pass);
       retract_board(moves.data[i]);
    }
    free(moves.data);
@@ -23,7 +28,16 @@ int64_t perft(int32_t depth, int64_t side) {
    return nmoves;
 }
 
-int64_t perft_capture(int32_t depth, int64_t side) {
+int64_t perft(int32_t depth) {
+   return impl_perft(depth, trunk.side);
+}
+
+/*!
+ * impl_perft_capture
+ * @ internal implementation of perft_capture
+ */
+
+int64_t impl_perft_capture(int32_t depth, int64_t side) {
    if (!depth) { return 1; }
 
    int64_t nmoves = 0;
@@ -31,10 +45,14 @@ int64_t perft_capture(int32_t depth, int64_t side) {
    for (int64_t i = 0; i != moves.count; ++i) {
       advance_board(moves.data[i]);
       if (!in_check(side))
-         nmoves += perft_capture(depth - 1, side ^ pass);
+         nmoves += impl_perft_capture(depth - 1, side ^ pass);
       retract_board(moves.data[i]);
    }
    free(moves.data);
 
    return nmoves;
+}
+
+int64_t perft_capture(int32_t depth) {
+   return impl_perft_capture(depth, trunk.side);
 }
