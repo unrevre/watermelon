214139e98d6ceadcffed75a042f645dd6bc00f3f
unrevre <unrevre@gmail.com>
2019-04-27 23:23:34 -0400

    inlines: add bsf/bsr functions compilable with gcc
    work around gcc errors/warnings with nominal implementation. the
    replacement functions 'waste' a few instructions by not taking advantage
    of the zero flag set by the bsfq/bsrq instructions.

--- src/inlines.h
+++ src/inlines.h
@@ -3,6 +3,44 @@
 
 #include <stdint.h>
 
+#ifndef __clang__
+/*!
+ * bsfq
+ * @ assembly instrinsic for bsfq
+ */
+
+__inline__ uint64_t bsfq(uint64_t bits) {
+   uint64_t index;
+   asm ("                  \n\
+        bsfq   %1, %0      \n\
+        "
+        : "=rm" (index)
+        : "rm" (bits)
+        :
+   );
+
+   return index;
+}
+
+/*!
+ * bsrq
+ * @ assembly instrinsic for bsrq
+ */
+
+__inline__ uint64_t bsrq(uint64_t bits) {
+   uint64_t index;
+   asm ("                  \n\
+        bsrq   %1, %0      \n\
+        "
+        : "=rm" (index)
+        : "rm" (bits)
+        :
+   );
+
+   return index;
+}
+#endif /* __clang__ */
+
 /*!
  * bsf
  * @ returns index of least significant bit of __uint128_t
@@ -10,8 +48,17 @@
 
 __inline__ uint64_t bsf(__uint128_t bits) {
    uint64_t clobber;
-
    uint64_t index;
+#ifndef __clang__
+   uint64_t high = bits >> 64;
+   uint64_t low = bits;
+
+   clobber = bsfq(high);
+   index = high ? clobber : 64;
+   index = index + 64;
+   clobber = bsfq(low);
+   index = low ? clobber : index;
+#else
    asm ("                  \n\
         movq   $64, %0     \n\
         bsfq   %3, %1      \n\
@@ -24,6 +71,7 @@ __inline__ uint64_t bsf(__uint128_t bits) {
         : "r" (bits), "r" (bits >> 64)
         :
    );
+#endif /* __clang__ */
 
    return index;
 }
@@ -35,8 +83,18 @@ __inline__ uint64_t bsf(__uint128_t bits) {
 
 __inline__ uint64_t bsr(__uint128_t bits) {
    uint64_t clobber;
-
    uint64_t index;
+#ifndef __clang__
+   uint64_t high = bits >> 64;
+   uint64_t low = bits;
+
+   index = bsrq(low);
+   clobber = low ? index : 128;
+   clobber = clobber ^ 64;
+   index = bsrq(high);
+   index = !high ? clobber : index;
+   index = index ^ 64;
+#else
    asm ("                  \n\
         movq   $128, %1    \n\
         bsrq   %2, %0      \n\
@@ -50,6 +108,7 @@ __inline__ uint64_t bsr(__uint128_t bits) {
         : "r" (bits), "r" (bits >> 64)
         :
    );
+#endif /* __clang__ */
 
    return index;
 }
