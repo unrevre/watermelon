3638a6285043622a093751c33a3fafe53b6e4dd8
unrevre <unrevre@gmail.com>
2019-05-16 10:16:23 -0400

    fen: optimise fen string parsing
    fun with fallthroughs.

--- src/fen.c
+++ src/fen.c
@@ -21,46 +21,43 @@ void reset_fen(const char* fen_str) {
    if (!fen_str) { fen_str = fen_default; }
    sscanf(fen_str, "%s %c", fstr, &schar);
 
+   trunk.side = (schar == 'b') ? black : red;
+
    for (int64_t i = 0; i != PIECES; ++i)
       trunk.pieces[i] = 0x0;
+   for (int64_t i = 0; i != 2; ++i)
+      trunk.occupancy[i] = 0x0;
    for (int64_t i = 0; i != POINTS; ++i)
       trunk.board[i] = empty;
 
    char* fstr_p = fstr;
    for (int64_t i = to_internal(0, HEIGHT - 1); *fstr_p; ++fstr_p, ++i) {
-      int32_t piece = -1;
+      int64_t p = 0;
 
       switch (*fstr_p) {
+         case '/':
+            i = i - FILES - WIDTH - 1;
+            break;
          case '1': case '2': case '3': case '4': case '5':
          case '6': case '7': case '8': case '9':
-            i += *fstr_p - '1'; break;
-         case 'k': case 'K': piece = 0x0; break;
-         case 'r': case 'R': piece = 0x1; break;
-         case 'n': case 'N': piece = 0x2; break;
-         case 'c': case 'C': piece = 0x3; break;
-         case 'a': case 'A': piece = 0x4; break;
-         case 'b': case 'B': piece = 0x5; break;
-         case 'p': case 'P': piece = 0x6; break;
-         case '/': i = i - FILES - WIDTH - 1; break;
-      }
-
-      if (piece != -1) {
-         int64_t side = (*fstr_p > 'Z');
-         trunk.board[i] = ps(side, piece);
+            i += *fstr_p - '1';
+            break;
+         case 'p': case 'P': ++p;
+         case 'b': case 'B': ++p;
+         case 'a': case 'A': ++p;
+         case 'c': case 'C': ++p;
+         case 'n': case 'N': ++p;
+         case 'r': case 'R': ++p;
+         case 'k': case 'K': ;
+            int64_t side = *fstr_p > 'Z';
+            uint32_t piece = ps(side, p);
+            trunk.pieces[piece] |= PMASK[i];
+            trunk.occupancy[side] |= PMASK[i];
+            trunk.board[i] = piece;
       }
    }
 
-   trunk.occupancy[0] = 0x0;
-   trunk.occupancy[1] = 0x0;
-
-   for (int64_t i = 0; i != POINTS; ++i) {
-      uint32_t piece = trunk.board[i];
-      trunk.pieces[piece] |= PMASK[i];
-      if (piece != empty) {
-         trunk.occupancy[s(piece)] |= PMASK[i]; }
-   }
-
-   trunk.side = (schar == 'b') ? black : red;
+   trunk.pieces[empty] = ~(trunk.occupancy[0] | trunk.occupancy[1]);
 }
 
 int64_t side_from(char const* fen_str) {
